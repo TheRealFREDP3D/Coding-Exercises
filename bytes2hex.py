e6ac3b8a05d6cabd50058eba688db10d82f556c9
TheRealFREDP3D <fredp3d@proton.me>
2024-01-28 15:34:03 -0500

    Added bytes2hex Added hex2bytes

---
+++ bytes2hex.py
@@ -0,0 +1,8 @@
+# Define byte data representing "Hello World"
+byte_data = b'Hello World'
+
+# Convert the byte data to a hexadecimal string using the .hex() instance method
+hex_string = byte_data.hex()
+
+# Print the hexadecimal string
+print("Hexadecimal string:", hex_string)
