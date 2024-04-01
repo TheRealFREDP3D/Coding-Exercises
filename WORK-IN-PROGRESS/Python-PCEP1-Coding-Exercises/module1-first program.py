4863c64b713aaedd059dae26386c97247e8676ef
TheRealFREDP3D <fredp3d@proton.me>
2024-04-01 10:27:11 -0400

    Added a lot of files -Courses Exercises -Personnal Projects -Some tests -Rework some programs -Level Up Organization

---
+++ WORK-IN-PROGRESS/Python-PCEP1-Coding-Exercises/module1-first program.py
@@ -0,0 +1,35 @@
+# Coding Exercises
+
+---
+
+> A good way to learn coding is to code. 
+
+---
+
+Here are the outcomes from my coding experimentation, encompassing activities undertaken during coding courses, engagements in CTF and diverse Challenges.
+
+---
+
+## Why I Created this Repository
+
+I am a big fan of the "Learn in Public" principles. It put some pressure to share good and working stuff. By sharing the results of my work, it creates networking opportunities. It can inspire people to start their own learning projects. It is a good way to show potential employer your skills or at least you appetite to learn. And finally, self-learners like me should have a way to monitor their progress.  
+
+- Make my work available to anyone, from anywhere.
+- Reference material and notes.
+- Prepare a publishable notebook.
+- Practice making good documentation.
+- Create content for GitBook, Wiki, Pages, Medium articles, etc.
+
+---
+
+## I needed a solution to solve a problem at:
+
+[https://CryptoHack.com](Cryptohack)
+[https://WeChall.net](WeChall)
+[https://Hackthebox.com](Hackthebox)
+[https://Tryhackme.com](Tryhackme)
+[https://pwn.college.com](pwn.college)
+[https://Hackviser.com](Hackviser)
+[https://Root-me.org](Root-me)
+[https://guyinatuxedo.github.io](Nightmare)
+[https://online.pwntilldawn.com](Pwntilldawn)
