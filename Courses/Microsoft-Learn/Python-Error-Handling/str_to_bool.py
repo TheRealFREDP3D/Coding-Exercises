6496ae610370a2aab6f0038c09a822506fb45e84
Frederick Pellerin <fredp3d@proton.me>
2024-04-27 02:53:24 -0400

    new file:   .templates/header.py new file:   Challenges/Cryptohack/xor-str.py new file:   Courses/Microsoft-Learn/Python-Error-Handling/str_to_bool.py deleted:    Templates/Header.py

---
+++ Courses/Microsoft-Learn/Python-Error-Handling/str_to_bool.py
@@ -0,0 +1,34 @@
+# ---------------------------------------------------------------------------- #
+# str_to_bool.py - Python 3.x.x ---------------------------------------------- #
+# Author: Frederick Pellerin ------------------------------------------------- #
+# GitHub: https://github.com/TheRealFREDP3D ---------------------------------- #
+# Twitter: https://twitter.com/TheRealFredP3D -------------------------------- #
+# ---------------------------------------------------------------------------- #
+# str_to_bool() -------------------------------------------------------------- #
+# This function takes a string value as input and attempts to convert it into- #
+# a boolean. ----------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+
+true_values = ['yes', 'y']
+false_values = ['no', 'n']
+
+def str_to_bool(value):
+    
+    value = value.lower()
+    if value in true_values:
+        return True
+    elif value in false_values:
+        return False
+    else:
+        raise ValueError('Invalid entry')
+    
+# Example usage
+print(str_to_bool('y'))
+print(str_to_bool('YES'))
+print(str_to_bool('n'))
+print(str_to_bool('NO'))
+print(str_to_bool('maybe'))
+
+
+
+
