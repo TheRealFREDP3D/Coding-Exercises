e14b56a153766dd2be4b320dbddacfba2f82f83c
Frederick Pellerin <fredp3d@proton.me>
2024-05-19 13:30:51 -0400

    new file:   Pwn.college/.vscode/settings.json renamed:    Pwn.college/SOLUTIONS/10.sh -> Pwn.college/Program-interaction/10.sh renamed:    Pwn.college/SOLUTIONS/11.sh -> Pwn.college/Program-interaction/11.sh renamed:    Pwn.college/SOLUTIONS/12.sh -> Pwn.college/Program-interaction/12.sh renamed:    Pwn.college/SOLUTIONS/13.sh -> Pwn.college/Program-interaction/13.sh renamed:    Pwn.college/SOLUTIONS/14.sh -> Pwn.college/Program-interaction/14.sh renamed:    Pwn.college/SOLUTIONS/15.py -> Pwn.college/Program-interaction/15.py renamed:    Pwn.college/SOLUTIONS/16.py -> Pwn.college/Program-interaction/16.py renamed:    Pwn.college/SOLUTIONS/17.py -> Pwn.college/Program-interaction/17.py renamed:    Pwn.college/SOLUTIONS/18.py -> Pwn.college/Program-interaction/18.py new file:   Pwn.college/Program-interaction/19.py new file:   Pwn.college/Program-interaction/20.py new file:   Pwn.college/Program-interaction/21.py renamed:    Pwn.college/SOLUTIONS/7.sh -> Pwn.college/Program-interaction/7.sh deleted:    Pwn.college/SOLUTIONS/19.py renamed:    complex-http-post-curl.sh -> Pwn.college/Talking-Web/complex-http-post-curl.sh renamed:    http-get-request.req -> Pwn.college/Talking-Web/http-get-request.req renamed:    http-post-request.py -> Pwn.college/Talking-Web/http-post-request.py renamed:    simple-http-post-request.txt -> Pwn.college/Talking-Web/simple-http-post-request.txt

---
+++ Pwn.college/Program-interaction/21.py
@@ -0,0 +1,3 @@
+# - the challenge checks for a specific parent process : ipython
+# - the challenge will check that the environment is empty (except LC_CTYPE, which is impossible to get rid of in some cases)
+
